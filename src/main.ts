99ecb126997fd88d36a1078bbbeb93088462e4ac
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2025-03-01 18:13:09 +0530

    config the awd in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'aws-sdk';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //setup the aws sdk
+  const configService = app.get(ConfigService);
+  config.update({
+    credentials: {
+      accessKeyId: configService.get('appConfig.awsAccessKeyId'),
+      secretAccessKey: configService.get('appConfig.awsSecretAccessKey'),
+    },
+    region: configService.get('appConfig.awsRegion'),
+  });
+
   await app.listen(3000);
 }
 bootstrap();
