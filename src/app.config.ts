d02b86453b99e14990f6c0105c1cced469ee167b
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2025-03-01 18:01:58 +0530

    create aws key configurations

---
+++ src/app.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('appConfig', () => ({
+  awsBucketName: process.env.AWS_PUBLIC_BUCKET_NAME,
+  awsRegion: process.env.AWS_REGION,
+  awsCloudfrontUrl: process.env.AWS_CLOUDFRONT_URL,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+}));
