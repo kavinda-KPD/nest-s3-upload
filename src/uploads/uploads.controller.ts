812ce8a0795dfa6ba9c28f69f18baca30373d6ee
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2025-03-01 18:50:15 +0530

    create the upload controller

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('uploads')
-export class UploadsController {}
+export class UploadsController {
+  constructor() {}
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('/file')
+  public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+
+    return {
+      originalname: file.originalname,
+      filename: file.filename,
+    };
+  }
+}
